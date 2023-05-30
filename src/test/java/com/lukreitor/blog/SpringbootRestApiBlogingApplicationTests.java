4e8f6e58c1db218ac32545ad53504ce7f618dfd3
Lucas Martins <lucassena@alunos.utfpr.edu.br>
2023-05-30 11:55:34 -0300

    initial project

---
+++ src/test/java/com/lukreitor/blog/SpringbootRestApiBlogingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukreitor.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiBlogingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
